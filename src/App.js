329c391e3eb96b486e465f3980b55a4149db4488
ayushsanjdev <ayushsanjpro@gmail.com>
2021-05-22 22:40:14 +0530

    done: for now, features left

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-// import ListComponent  from "./components/ListComponent";
-import { db } from './firebaseConfig';
-import firebase from 'firebase/app';
-
-
-
+import { db } from "./firebaseConfig";
+import firebase from "firebase/app";
 
 function App() {
-
   //TODOS-FEATURES
   //take input from user with prompt..
   //show it in the available balance..
@@ -17,46 +12,23 @@ function App() {
   const [totalExpense, setTotalExpense] = useState(0);
   const [amount, setAmount] = useState();
   const [item, setItem] = useState("");
-  const spanEl = useRef();
+  const [allExpenses, setAllExpenses] = useState(null);
 
-  // useEffect(() => {
-  //   getDocs();
-  // }, [])
-
-  const getDocs = () => {
+  useEffect(() => {
     db.collection("expenseUser")
-    .orderBy("createdAt", "desc")
-    .onSnapshot((snap) => {
-      let documents = [];
-      snap.forEach((doc) => {
-        if (doc.id === "total") {
-          spanEl.current.textContent = doc.data().total;
-        } else {
-          documents.push({
-            ...doc.data(),
-            docId: doc.id,
-          });
-        }
-        renderList(documents);
-        // console.log(documents);
-      })
-    });
-  }
-
-  const renderList = (arrOfList) => {
-    spanEl.current.textContent = arrOfList.map((amount) => createListItem(amount))
-    .join("");
-  }
-
-  const createListItem = ({desc, amount, createdAt, docId}) => {
-    return (
-      <ul>
-        `<li>${desc, amount, createdAt, docId}</li>`
-      </ul>
-    )
-  }
-  
-
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          if (doc.id === "total") {
+            setTotalExpense(doc.data().total);
+          } else {
+            documents.push({ ...doc.data(), docId: doc.id });
+          }
+        });
+        setAllExpenses(documents);
+      });
+  }, []);
 
   // const deleteDocs = (docId, totalexpense) => {
   //   db.collection("expenseUser")
@@ -75,33 +47,30 @@ function App() {
   //   });
   // }
 
-
-  // const addDocs = (textDesc, amount) => {
-  //   db.collection("expenseUser")
-  //   .add({
-  //     desc: textDesc,
-  //     amount: amount,
-  //     createdAt: firebase.firestore.FieldValue.serverTimestamp()
-  //   })
-  //   .then((docRef) => {
-  //     console.log("doc id: ", docRef.id);
-  //   })
-  //   .catch((error) => {
-  //     console.error("error adding document: ", error);
-  //   });
-  //   db.collection("expenseUser")
-  //   .doc("total")
-  //   .update({
-  //     total: firebase.firestore.FieldValue.increment(amount)
-  //   });
-  // }
-
+  const addDocs = (textDesc, amount) => {
+    db.collection("expenseUser")
+      .add({
+        desc: textDesc,
+        amount: amount,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+      .then((docRef) => {
+        console.log("doc id: ", docRef.id);
+      })
+      .catch((error) => {
+        console.error("error adding document: ", error);
+      });
+    db.collection("expenseUser")
+      .doc("total")
+      .update({
+        total: firebase.firestore.FieldValue.increment(amount)
+      });
+  };
 
   const setExpense = () => {
-    setTotalExpense(
-      (prevExpense) => parseInt(amount, 10) + prevExpense);
-    getDocs();
-  }
+    setAmount(parseInt(amount, 10));
+    addDocs(item, amount);
+  };
 
   // const renderList = (arr) => {
   //   arr.map((amount) => createListItem(amount));
@@ -113,40 +82,47 @@ function App() {
 
   const handleChange = (e) => {
     setAmount(e.target.value);
-  }
+  };
 
   const itemHandleChange = (e) => {
     setItem(e.target.value);
-  }
+  };
 
   return (
     <div className="app">
       <header>
-        <h1>
-          Expense Management App
-        </h1>
+        <h1>Expense Management App</h1>
         <h3>
-          Total Spent: 
-          <span ref={spanEl}>{totalExpense}</span> <br />
-          Item Name:  
-          <span> {item}</span>
+          Total Spent:
+          <span>{totalExpense}</span> <br />
         </h3>
       </header>
-        <input
-          type="text"
-          value={amount}
-          onChange={handleChange}
-          placeholder="Today's expense..." />
-        <input
-          type="text"
-          value={item}
-          placeholder="Item Name..."
-          onChange={itemHandleChange}
-        />
-      <button onClick={setExpense}>Add</button>
-      <section>
-          <li ref={spanEl}>{}</li>
-      </section>
+      <input
+        type="text"
+        value={amount}
+        onChange={handleChange}
+        placeholder="Today's expense..."
+      />
+      <input
+        type="text"
+        value={item}
+        placeholder="Item Name..."
+        onChange={itemHandleChange}
+      />
+      <button onClick={setExpense}>++</button>
+      <p>Your expense history: </p>
+      <ul>
+        {allExpenses &&
+          allExpenses.map((expense) => {
+            return (
+              <li key={expense.docId}>
+                <span className="first-span">{expense.amount}</span>
+                {"  "}
+                <span className="second-span">{expense.desc}</span>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 }
